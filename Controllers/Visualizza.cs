4c83608a8b6a9f8124b54ab79966144ba102b883
Riccardo Cannella <riccardo.cannella@studenti.unicam.it>
2018-03-31 15:03:52 +0200

    Scheletro dell'app, prove generali

---
+++ Controllers/Visualizza.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using webmva.Models;
+
+namespace webmva.Controllers
+{
+    public class Visualizza: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+       
+    }
+}
--- config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url : 'mongodb://admin:Unicam!2018$@ds143388.mlab.com:43388/webmva'
-};
