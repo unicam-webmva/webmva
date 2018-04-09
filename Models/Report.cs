0de68ca52747fba92f847df1e22f4c608835817b
MargheritaRenieri <margherita.renieri@studenti.unicam.it>
2018-04-09 10:26:51 +0200

    inserimento pagina per la creazione di un nuovo progetto, icreazione pagina report

---
+++ Models/Report.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace webmva.Models
+{
+    /// <summary>
+    /// Questo modello rappresenta le entità Report
+    /// </summary>
+    public class Report
+    {
+   
+        
+    }
+}
