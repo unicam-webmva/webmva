b991b487f77f2a372eef055e47ec9a46448a879e
Riccardo Cannella <riccardo.cannella@studenti.unicam.it>
2018-05-18 11:35:27 +0200

    Bugfix: tolta una parentesi di troppo

--- Data/MyDbContext.cs
+++ Data/MyDbContext.cs
@@ -6,20 +6,20 @@ namespace webmva.Data
     public class MyDbContext : DbContext
     {
         public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
-        
+
         //QUI CI VANNO LE TABELLE DEL DB
         public DbSet<Modulo> Moduli { get; set; }
-        
-        public DbSet<Progetto> Progetti { get; set; }
-        public DbSet<ModuliProgetto> ModuliProgetto {get; set;}
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder){
+        public DbSet<Progetto> Progetti { get; set; }
+        public DbSet<ModuliProgetto> ModuliProgetto { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
             modelBuilder.Entity<ModuloNMAP>().HasBaseType<Modulo>();
             modelBuilder.Entity<ModuloNESSUS>().HasBaseType<Modulo>();
             modelBuilder.Entity<ModuloDNSRECON>().HasBaseType<Modulo>();
             modelBuilder.Entity<ModuloDROOPE>().HasBaseType<Modulo>();
             modelBuilder.Entity<ModuloINFOGA>().HasBaseType<Modulo>();
-        }
         }
     }
 }
