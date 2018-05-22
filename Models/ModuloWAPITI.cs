daf29d7c604d4405c1ec7fb94c0d069d36606bcb
MargheRIta <margherita.renieri@studenti.unicam.it>
2018-05-22 11:52:58 +0200

    modulo wapiti nuovo

--- Models/ModuloWAPITI.cs
+++ Models/ModuloWAPITI.cs
@@ -5,133 +5,73 @@ using System.Threading.Tasks;
 
 namespace webmva.Models
 {
-public enum SCOPE{
+    public enum SCOPE
+    {
         PAGE, FOLDER, DOMAIN, URL, NESSUNO
-     }
-public enum FORCE{
+    }
+    public enum FORCE
+    {
         PARANOID, SNEAKY, POLITE, AGGRESSIVE, INSANE, NORMAL
-     }
-public enum VERBOSEWAPITI{
+    }
+    public enum VERBOSEWAPITI
+    {
         ZERO, UNO, DUE
-     }
+    }
     public class ModuloWAPITI : Modulo
     {
         public string URL { get; set; }
-        private bool NotAll= false;
-        private bool _all = true;
-        public bool All{get{return _all;}
-        set{
-            if(value)
-            NotAll=false;
-            else
-             NotAll=true;
-            _all = value;
-        }}
-        private bool _backUp = false;
-        public bool BackUp { get{
-            return _backUp;
-        } set{
-            NotAll=true;
-            _backUp=value;
-        } }
-         private bool _blindSql = false;
-        public bool BlindSql { get{
-            return _blindSql;
-        } set{
-            NotAll=true;
-            _blindSql=value;
-        } }
-         private bool _crlf = false;
-        public bool Crlf  { get{
-            return _crlf;
-        } set{
-            NotAll=true;
-            _crlf=value;
-        } }
-        private bool _exec = false;
-        public bool Exec  { get{
-            return _exec;
-        } set{
-            NotAll=true;
-            _exec=value;
-        } }
-        private bool _file = false;
-        public bool File { get{
-            return _file;
-        } set{
-            NotAll=true;
-            _file=value;
-        } }
-        private bool _htAccess = false;
-        public bool Htaccess { get{
-            return _htAccess;
-        } set{
-            NotAll=true;
-            _htAccess=value;
-        } }  
-        private bool _nikto = false;      
-        public bool Nikto  { get{
-            return _nikto;
-        } set{
-            NotAll=true;
-            _nikto=value;
-        } }
-        private bool _permanentXss = false;     
-        public bool PermanentXss { get{
-            return  _permanentXss;
-        } set{
-            NotAll=true;
-             _permanentXss=value;
-        } }
-        private bool _sql = false;     
-        public bool Sql  { get{
-            return _sql;
-        } set{
-            NotAll=true;
-            _sql=value;
-        } }
-        private bool _xss = false;  
-        public bool Xss  { get{
-            return _xss;
-        } set{
-            NotAll=true;
-            _xss=value;
-        } }
-        private bool _buster = false;  
-        public bool Buster  { get{
-            return _buster;
-        } set{
-            NotAll=true;
-            _buster=value;
-        } }
-        private bool _shellshock = false;
-        public bool ShellShock  { get{
-            return _shellshock;
-        } set{
-            NotAll=true;
-            _shellshock=value;
-        } }
 
-      private SCOPE _Scope= SCOPE.NESSUNO;
-        public SCOPE Scope{
-            get {return _Scope;}
-            set{ _Scope=value;}
+        public bool All { get; set; }
+        private bool _common = true;
+        public bool Common
+        {
+            get { return _common; }
+            set { _common = value; }
         }
-    private FORCE _Force= FORCE.NORMAL;
-        public FORCE Force{
-            get {return _Force;}
-            set{ _Force=value;}
+        private bool _nessuno = false;
+        public bool Nessuno
+        {
+            get { return _nessuno; }
+            set { _nessuno = value; }
         }
-    private VERBOSEWAPITI _Verbose= VERBOSEWAPITI.UNO;
-        public VERBOSEWAPITI Verbose{
-            get {return _Verbose;}
-            set{ _Verbose =value;}
+
+        public bool BackUp { get; set; }
+        public bool BlindSql { get; set; }
+        public bool Crlf { get; set; }
+        public bool Exec { get; set; }
+        public bool File { get; set; }
+        public bool Htaccess { get; set; }
+        public bool Nikto { get; set; }
+        public bool PermanentXss { get; set; }
+        public bool Sql { get; set; }
+        public bool Xss { get; set; }
+        public bool Buster { get; set; }
+        public bool ShellShock { get; set; }
+
+        private SCOPE _Scope = SCOPE.NESSUNO;
+        public SCOPE Scope
+        {
+            get { return _Scope; }
+            set { _Scope = value; }
         }
-       public int MaxMinutes{
-           get{return _max;}
-           set{_max=value;}
-       }
-       private int _max= -1;
+        private FORCE _Force = FORCE.NORMAL;
+        public FORCE Force
+        {
+            get { return _Force; }
+            set { _Force = value; }
+        }
+        private VERBOSEWAPITI _Verbose = VERBOSEWAPITI.UNO;
+        public VERBOSEWAPITI Verbose
+        {
+            get { return _Verbose; }
+            set { _Verbose = value; }
+        }
+        public int MaxMinutes
+        {
+            get { return _max; }
+            set { _max = value; }
+        }
+        private int _max = -1;
         public string ComandoPersonalizzato { get; set; }
         public override string Comando
         {
@@ -139,29 +79,32 @@ public enum VERBOSEWAPITI{
             {
                 if (string.IsNullOrEmpty(ComandoPersonalizzato))
                 {
-                    
-                    string risultato ="wapiti.py" +" --color" ;
 
-                  if (NotAll){
-                    risultato+=" -m \"-all";
-                   if (BackUp) risultato +=",backup";
-                   if (BlindSql) risultato +=",blindsql";
-                   if (Crlf) risultato +=",crlf";
-                   if (Exec) risultato +=",exec";
-                   if (File) risultato +=",file";
-                   if (Htaccess) risultato +=",htaccess";
-                   if (Nikto) risultato +=",nikto";
-                   if (PermanentXss) risultato +=",permanentxss";
-                   if (Sql) risultato +=",sql";
-                   if (Xss) risultato +=",xss";
-                   if (Buster) risultato +=",buster";
-                   if (ShellShock) risultato +=",shellshock";
-                   risultato+="\"";
-                  }
-                if(_max!=-1){
-                risultato+="--max-scan-time "+MaxMinutes;
-                }
-                switch (Force)
+                    string risultato = "wapiti.py" + " --color";
+                    if (Nessuno) risultato += " -m \"\"";
+                    else if (!All)
+                    {
+                        risultato += " -m \"-all";
+                        if (Common) risultato += ",common";
+                        if (BackUp) risultato += ",backup";
+                        if (BlindSql) risultato += ",blindsql";
+                        if (Crlf) risultato += ",crlf";
+                        if (Exec) risultato += ",exec";
+                        if (File) risultato += ",file";
+                        if (Htaccess) risultato += ",htaccess";
+                        if (Nikto) risultato += ",nikto";
+                        if (PermanentXss) risultato += ",permanentxss";
+                        if (Sql) risultato += ",sql";
+                        if (Xss) risultato += ",xss";
+                        if (Buster) risultato += ",buster";
+                        if (ShellShock) risultato += ",shellshock";
+                        risultato += "\"";
+                    }
+                    if (_max != -1)
+                    {
+                        risultato += "--max-scan-time " + MaxMinutes;
+                    }
+                    switch (Force)
                     {
                         case FORCE.PARANOID:
                             risultato += " -S paranoid";
@@ -180,12 +123,12 @@ public enum VERBOSEWAPITI{
                             break;
                         default: break;
                     }
-                switch (Verbose)
+                    switch (Verbose)
                     {
                         case VERBOSEWAPITI.ZERO:
                             risultato += " -v 0";
                             break;
-                         case VERBOSEWAPITI.DUE:
+                        case VERBOSEWAPITI.DUE:
                             risultato += " -v 2";
                             break;
                         default: break;
